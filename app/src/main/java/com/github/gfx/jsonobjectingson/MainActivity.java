e98676b88c8e50d7128a258b4b1cfc9f7c5b1af9
FUJI Goro (gfx) <gfuji@cpan.org>
2015-06-16 22:17:21 +0900

    add benchark for parsing

--- app/src/main/java/com/github/gfx/jsonobjectingson/MainActivity.java
+++ app/src/main/java/com/github/gfx/jsonobjectingson/MainActivity.java
@@ -1,37 +1,97 @@
 package com.github.gfx.jsonobjectingson;
 
-import android.support.v7.app.AppCompatActivity;
+import com.google.common.io.CharStreams;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 public class MainActivity extends AppCompatActivity {
+
+    String jsonString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        InputStream is = getResources().openRawResource(R.raw.qiita);
+        try {
+            jsonString = CharStreams.toString(new InputStreamReader(is, Charset.forName("UTF-8")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                runJson();
+            }
+        }, 1000);
+    }
+
+    void runJson() {
+        new AsyncTask<Void, Void, Long>() {
+            @Override
+            protected Long doInBackground(Void... params) {
+                long t0 = System.currentTimeMillis();
+
+                try {
+                    JSONArray json = new JSONArray(jsonString);
+                    Log.d("XXX", "length: " + json.length());
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+                return System.currentTimeMillis() - t0;
+            }
+
+            @Override
+            protected void onPostExecute(Long elapsed) {
+                TextView textView = (TextView) findViewById(R.id.json);
+                textView.setText("Parse with JSONObject: " + elapsed + "ms");
+
+                runGson();
+            }
+        }.execute();
+    }
+
+    void runGson() {
+        new AsyncTask<Void, Void, Long>() {
+            @Override
+            protected Long doInBackground(Void... params) {
+                long t0 = System.currentTimeMillis();
+
+                JsonArray json = (JsonArray) new JsonParser().parse(jsonString);
+                Log.d("XXX", "size: " + json.size());
+
+                return System.currentTimeMillis() - t0;
+            }
+
+            @Override
+            protected void onPostExecute(Long elapsed) {
+                TextView textView = (TextView) findViewById(R.id.gson);
+                textView.setText("Parse with Gson: " + elapsed + "ms");
+            }
+        }.execute();
+
     }
 }
